22430ea35db9be25f7818c9e1d15a0b4b3358201
add33m <48681209+add33m@users.noreply.github.com>
2022-02-01 15:34:28 +0100

    Fixed snake ish

--- screen/snake.py
+++ screen/snake.py
@@ -1,8 +1,10 @@
 from ast import Str
+import math
 import os
 import socket as soc
 import time
 import random
+from xml.etree.ElementTree import tostring
 
 HOST = "skarm.ludd.ltu.se"
 PORT = 1337
@@ -10,6 +12,9 @@ PORT = 1337
 # Size of the screen: 1024x576
 SCREEN_MAX_X = 1024
 SCREEN_MAX_Y = 576
+SCALE = 64
+MAX_X = math.floor(SCREEN_MAX_X/SCALE)
+MAX_Y = math.floor(SCREEN_MAX_Y/SCALE)
 
 # Snake is an array of snakeparts
 class SnakePart:
@@ -23,41 +28,44 @@ class SnakePart:
 
 # Setup snake
 snakeArray = []
-age = 1
+age = 5
 heading = 1     # 0 = up, 1 = right, 2 = down, 3 = left
 
 def reset():
   age = 1
-  snakeArray[:] = [SnakePart(SCREEN_MAX_X/2, SCREEN_MAX_Y/2, age)]
+  snakeArray[:] = [SnakePart(math.floor(MAX_X/2), math.floor(MAX_Y/2), age)]
 
 def tick():
   for (i, part) in enumerate(snakeArray):
     part.age()
-    if part.life == 0:
-      del snakeArray[i]
+  if (snakeArray[0].life <= 0):
+    snakeArray.pop(0)
 
 def move(direction):
   # Move snake forward by adding a new snakepart with age + 1 (to cancel out the coming tick)
   if direction == 0:
-    snakeArray.insert(0, SnakePart(snakeArray[-1].x, snakeArray[-1].y - 1, age + 1))
+    snakeArray.append(SnakePart(snakeArray[-1].x,     snakeArray[-1].y - 1, age + 1))
   elif direction == 1:
-    snakeArray.insert(0, SnakePart(snakeArray[-1].x + 1, snakeArray[-1].y, age + 1))
+    snakeArray.append(SnakePart(snakeArray[-1].x + 1, snakeArray[-1].y,     age + 1))
   elif direction == 2:
-    snakeArray.insert(0, SnakePart(snakeArray[-1].x, snakeArray[-1].y + 1, age + 1))
+    snakeArray.append(SnakePart(snakeArray[-1].x,     snakeArray[-1].y + 1, age + 1))
   elif direction == 3:
-    snakeArray.insert(0, SnakePart(snakeArray[-1].x - 1, snakeArray[-1].y, age + 1))
+    snakeArray.append(SnakePart(snakeArray[-1].x - 1, snakeArray[-1].y,     age + 1))
 
 def renderPart(part):
+  print(part.x, part.y, part.life)
   renderString = ""
-  color = Str(random.randrange(0, 10))*6 # Random greyscale color
+  color = str(random.randrange(0, 10))*6 # Random greyscale color
   # Make a square
-  for x in range(0, 20):
-    for y in range(0, 20):
+  for x in range(0, SCALE):
+    for y in range(0, SCALE):
       # Draw border white
-      if x == 0 or y == 0 or x == 19 or y == 19:
-        renderString += f"PX {part.x + x} {part.y + y} FFFFFF\n"
+      if x == 0 or y == 0 or x == SCALE-1 or y == SCALE-1:
+        renderString += f"PX {part.x*SCALE + x} {part.y*SCALE + y} FFFFFF\n"
       else:
-        renderString += f"PX {part.x + x} {part.y + y} {color}\n"
+        renderString += f"PX {part.x*SCALE + x} {part.y*SCALE + y} {color}\n"
+  
+  return renderString
 
 socket = soc.socket(soc.AF_INET, soc.SOCK_STREAM)
 socket.connect((HOST, PORT))
@@ -77,14 +85,20 @@ while True:
   move(heading)
 
   # Check if snake has hit itself
+  collided = False
   for part in snakeArray:
-    if part.x == snakeArray[0].x and part.y == snakeArray[0].y:
+    if part.life != snakeArray[-1].life and part.x == snakeArray[-1].x and part.y == snakeArray[-1].y:
       # Reset and restart loop
       reset()
-      continue
+      collided = True
+      break
+    
+  if collided:
+    print("collided")
+    continue
 
   # Check if snake has hit the screen border
-  if snakeArray[0].x < 0 or snakeArray[0].x >= SCREEN_MAX_X or snakeArray[0].y < 0 or snakeArray[0].y >= SCREEN_MAX_Y:
+  if snakeArray[-1].x < 0 or snakeArray[-1].x >= MAX_X or snakeArray[-1].y < 0 or snakeArray[-1].y >= MAX_Y:
     # Reset and restart loop
     reset()
     continue
@@ -99,4 +113,6 @@ while True:
 
   # Send output to screen
   socket.sendall(output.encode())
+
+  time.sleep(.5)
   
